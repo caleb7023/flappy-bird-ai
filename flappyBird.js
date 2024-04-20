3e7d6b9d19c9fc6f20b5f44ca445bd87df81ef33
caleb7023 <caleb70230@gmail.com>
2024-04-20 17:08:40 +0900

    added a brain

--- flappyBird.js
+++ flappyBird.js
@@ -5,7 +5,8 @@ HEIGHT = 400 // The height of the stage (pixel)
 GRAVITY = 0.1;
 
 class bird {
-    constructor (element) {
+    constructor (element, nn) {
+        this.nn         = nn;      // the neutral network
         this.posY       = 300;     // How height from ground
         this.element    = element; // The html element of the bird
         this.gameover   = false;   // was the bird gameovered or not
@@ -33,9 +34,8 @@ class pipe {
     };
 };
 
-moveBird = () => {
-    birds.forEach((targetBird) => {
-    });
+jumpBird = (targetBird) => {
+    targetBird.UpVelocity += 4;
 };
 
 gameProcess = () => {
@@ -45,7 +45,7 @@ gameProcess = () => {
             targetBird.posY       += targetBird.UpVelocity; // move by applying the velocity
             if (targetBird.posY < 0 || 400 < targetBird.posY) { // check is the bird touched to bottom, top or the pipe.
                 targetBird.gameover = true;
-                targetBird.posY     = 0;
+                targetBird.posY     = targetBird.posY < 0 ? 0 : HEIGHT-68;
                 targetBird.element.style.filter = "grayscale(1)";
             };
         };
@@ -87,9 +87,25 @@ newNeurons = (amount, inputAmount) => {
 
 train = (LearningRate) => {
 
-    firstLayerNeurons  = newNeurons(32, 4);
-    secondLayerNeurons = newNeurons(8, 32);
-    outputNeuron       = newNeurons(1, 8)
+    birds.forEach((targetBird) => {
+        if (!targetBird.gameover){
+        
+            input = [
+                targetBird.UpVelocity,
+                targetBird.posY,
+                0,
+                0
+            ];
+    
+            var firstLayerOutput  = targetBird.nn[0].map((array) =>     swish(sum(mulArray(array[0], input           )) + array[1]));
+            var secondLayerOutput = targetBird.nn[1].map((array) => Math.tanh(sum(mulArray(array[0], firstLayerOutput)) + array[1]));
+            var thirdLayerOutput  = sum(mulArray(targetBird.nn[2][0][0], secondLayerOutput)) + targetBird.nn[2][0][1]
+
+            if (0 < thirdLayerOutput){jumpBird(targetBird)};
+
+        };
+
+    });
 
 };
 
@@ -100,7 +116,11 @@ main = () => {
         birdElement_temp.setAttribute("src", "./Imgs/bird.png");
         birdElement_temp.style.bottom = "300px";
         birdElement = document.getElementById("birds").appendChild(birdElement_temp);
-        birds.push(new bird(birdElement));
+        birds.push(new bird(birdElement, [
+            newNeurons(32, 4 ), // first layer's neurons
+            newNeurons(8 , 32), // second layer's neurons
+            newNeurons(1 , 8 )  // output's neuron
+        ]));
     };
     
     delete birdElement_temp;
